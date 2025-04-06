b067d7c03cb2f70d6e897ffa95943301ea565ee0
Wouter Schoot <wouter@schoot.org>
2025-04-06 21:43:24 +0200

    update

--- telemetry.py
+++ telemetry.py
@@ -2,6 +2,8 @@ from os import listdir
 from os.path import isfile, join
 import matplotlib.pyplot as plt
 import gpxpy
+import moviepy as mp
+import json
 
 lat = []
 lon = []
@@ -15,8 +17,14 @@ fig = plt.figure(facecolor = '1')
 plt.axis('off') 
 # fig.add_axes(ax)
 
+frop = mp.VideoFileClip('20250327_131046.mp4')
+
+print( frop.reader.infos.get('metadata').get('creation_time'))
+
+print(frop.duration)
+
 gpx_filename = 'activity_18651877796.gpx'
-gpx_file = open(gpx_filename, 'r')
+gpx_file = open(gpx_filename, 'r', encoding='utf-8')
 gpx = gpxpy.parse(gpx_file)
 
 for track in gpx.tracks:
@@ -24,16 +32,16 @@ for track in gpx.tracks:
         for point in segment.points:
             lat.append(point.latitude)
             lon.append(point.longitude)
-    
+
+print(len(lat))
 plt.plot(lon, lat, color = 'red', lw = 1, alpha = 1)
 
 lat = []
 lon = []
 
 distance = gpx.length_2d()
-distance/=1609.344
 
-# plt.figtext(.5,.9,'Hiking in Colombia, 2019', fontsize=30, ha='center')
+plt.figtext(.5,.9,'Pace', fontsize=30, ha='center')
 # plt.figtext(.135,.85,'Days: 4',fontsize=15,ha='left',color='blue')
 # plt.figtext(.135,.82,'Miles: %i' %distance,fontsize=15,ha='left',color='blue')
 
@@ -43,5 +51,29 @@ distance/=1609.344
 plt.savefig('gpx_plot.png', dpi=300,transparent=True)
 
 # plt.savefig('gpx_plot.png', dpi=300, bbox_inches='tight')
-plt.show()
+# plt.show()
 gpx_file.close()
+
+# https://zulko.github.io/moviepy/getting_started/quick_presentation.html
+
+# Load file example.mp4 and extract only the subclip from 00:00:10 to 00:00:20
+
+
+# Generate a text clip. You can customize the font, color, etc.
+txt_clip = mp.TextClip(
+    font="Arial", text="km's: " + str(distance), font_size=70, color="white"
+)
+img_clip = mp.ImageClip("gpx_plot.png").with_duration(frop.duration)
+# Say that you want it to appear for 10s at the center of the screen
+txt_clip = txt_clip.with_position("center").with_duration(frop.duration)
+
+# Overlay the text clip on the first video clip
+video = mp.CompositeVideoClip([frop, txt_clip, img_clip])
+
+# Write the result to a file (many options available!)
+video.write_videofile("result.mp4")
+
+
+# print(frop.metadata)
+
+
